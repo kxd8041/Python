287618265a632fa9f7e77a2a8bca417462d01a81
kxd8041 <kxd8041@g.rit.edu>
2017-01-25 11:09:37 -0800

    Add files via upload

--- kriwitterthon/main.py
+++ kriwitterthon/main.py
@@ -2,22 +2,22 @@ from twython import Twython,TwythonError
 import linecache
 import os
 
-loc=os.getcwd()+"/t.txt"
-print linecache.getline(loc,2).rstrip()
 
 
 
 class kriwitterthon(object):
+
+
     def gettingmessages(self):
+        loc = os.getcwd() + "/t.txt"
 
         APP_KEY = "W3vcoOKqpj53QXZQZf0SXgkjz"
-
-
-        APP_SECRET = "a7iw3LspHUVQ5UvcIJG5q8Uh9obPs9VdvzcKCKfifpVwFIkq6K"
+        APP_SECRET = linecache.getline(loc, 2).rstrip()
 
         twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
         ACCESS_TOKEN = twitter.obtain_access_token()
         twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+
         user=raw_input("Please enter a valid twitter screen name:")
         try:
             for i in range(0, 10):
@@ -27,4 +27,47 @@ class kriwitterthon(object):
         except TwythonError as e:
             print e
 
-    gettingmessages(1)
+    def gettingUsers(self):
+        loc = os.getcwd() + "/t.txt"
+        APP_KEY = "W3vcoOKqpj53QXZQZf0SXgkjz"
+        APP_SECRET = linecache.getline(loc, 2).rstrip()
+
+        twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+        ACCESS_TOKEN = twitter.obtain_access_token()
+        twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+
+        user=raw_input("Please enter a valid twitter screen name:")
+        output = twitter.get_followers_ids(screen_name=user)
+
+        for x in output["ids"]:
+            data = twitter.show_user(user_id=x)
+            print(data["screen_name"])
+
+    def gettingsimilarUser(self):
+        loc = os.getcwd() + "/t.txt"
+        APP_KEY = "W3vcoOKqpj53QXZQZf0SXgkjz"
+        APP_SECRET = linecache.getline(loc, 2).rstrip()
+
+        twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+        ACCESS_TOKEN = twitter.obtain_access_token()
+        twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
+
+        user = raw_input("Please enter a valid twitter screen name:")
+        output = twitter.get_followers_ids(screen_name=user)
+        user2 = raw_input("Please enter another valid twitter screen name:")
+        output2 = twitter.get_followers_ids(screen_name=user)
+
+        for x in output["ids"]:
+            for y in output2["ids"]:
+                data = twitter.show_user(user_id=x)
+                data2 = twitter.show_user(user_id=y)
+                if data ==data2:
+                    print(data["screen_name"])
+
+
+
+
+
+    #gettingmessages(1)
+    #gettingUsers(1)
+    #gettingsimilarUser(1)
