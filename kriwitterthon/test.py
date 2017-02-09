aca46339caee9a5d9ba0d06a91a0cc69b71c4881
kxd8041 <kxd8041@g.rit.edu>
2017-02-09 07:49:54 -0800

    Add files via upload

--- kriwitterthon/test.py
+++ kriwitterthon/test.py
@@ -1,6 +1,6 @@
 import configparser
 from twython import Twython,TwythonError
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request, redirect, render_template
 app = Flask(__name__)
 
 Config = configparser.ConfigParser()
@@ -12,31 +12,40 @@ twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
 ACCESS_TOKEN = twitter.obtain_access_token()
 twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
 
-
 @app.route('/')
-def hello_world():
+def index():
+    return render_template('kriwitterthon.html')
+
+
+
+@app.route('/', methods = ['POST'])
+def Getting_Tweets():
 
     val = []
+    user = request.form['user']
+    print user
     for i in range(0, 10):
-        user_timeline = twitter.get_user_timeline(screen_name="krish1092", count=10)
+        user_timeline = twitter.get_user_timeline(screen_name=user, count=10)
         for twitteruser in user_timeline:
             val.append(twitteruser['text'])
 
     return jsonify(result= val )
 
-@app.route('/1')
+@app.route('/', methods = ['POST'])
 
 def Getting_Followers():
     try:
+        user = request.form['user1']
+        print user
         val =[]
         next_cursor = -1
         while next_cursor != 0:
-            output = twitter.get_followers_list(screen_name="krish1092", count=200, cursor=next_cursor)
+            output = twitter.get_followers_list(screen_name=user, count=200, cursor=next_cursor)
 
             for twitteruser in output["users"]:
                 val.append(twitteruser["screen_name"])
                 next_cursor = output["next_cursor"]
-            return jsonify(result=val)
+        return jsonify(result=val)
 
 
 
@@ -45,12 +54,14 @@ def Getting_Followers():
         print e
 
 
-@app.route('/2')
+@app.route('/', methods = ['POST'])
 
 def Getting_Similar_Followers():
     try:
+        user = request.form['user2']
+        print "hi"
         val = []
-        output = twitter.get_followers_ids(screen_name="krish1092")
+        output = twitter.get_followers_ids(screen_name= user)
         output2 =twitter.get_followers_ids(screen_name="krish1092")
 
         for twitteruser1 in output["ids"]:
@@ -64,15 +75,20 @@ def Getting_Similar_Followers():
         print e
         
 
-@app.route('/3')
+@app.route('/', methods = ['POST'])
 
 def Searching_Tweet():
     try:
+        user = request.form['user3']
+        string = request.form['string']
+        print user, string
+        print 'hi'
+
         val = []
-        output = twitter.get_user_timeline(screen_name="krish1092")
+        output = twitter.get_user_timeline(screen_name=user)
 
         for twitteruser in output:
-            if "adi" in twitteruser['text']:
+            if string in twitteruser['text']:
                 val.append(twitteruser['text'])
         return jsonify(result=val)
 
@@ -80,4 +96,5 @@ def Searching_Tweet():
         print e
 
 
-app.run()
+if __name__ == '__main__':
+    app.run()
